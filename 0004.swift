19cee7eb4b97ce56c31fefb5c28489afdb7f7753
Jollen Chen <jollen@moko365.com>
2014-08-16 10:51:28 +0800

    add 0004.swift

---
+++ 0004.swift
@@ -0,0 +1,11 @@
+#!/usr/bin/env DEVELOPER_DIR=/Applications/Xcode6-Beta3.app/Contents/Developer xcrun swift -i
+
+// C-style for-loop
+
+var i: Int
+
+for i = 0; i < 100; i++ {
+	println(i)
+}
+
+println(i)
